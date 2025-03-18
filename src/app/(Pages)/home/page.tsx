dd2bebdc183fb053023d2d8b3a9cee8c65047b63
Muhammad Talha <talha18513@gmail.com>
2025-03-18 18:36:28 +0500

    home page

--- src/app/(Pages)/home/page.tsx
+++ src/app/(Pages)/home/page.tsx
@@ -1,6 +1,7 @@
 import Wrapper from '@/app/Wrapper'
 import React from 'react'
 import img from '../../../Assets/robot.png'
+import img1 from '../../../Assets/img1.jpg'
 import Image from 'next/image'
 import Cards from '@/app/Components/Cards';
 import Link from 'next/link';
@@ -27,9 +28,20 @@ const Home = () => {
                 <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 md:mt-12 mt-8 md:gap-8 gap-4">
                     <Cards />
                 </div>
-                    <div className="flex items-center justify-center w-full mt-10">
-                        <Link href={"/services"} className='px-10 py-3 block bg-primary transition-all duration-500 hover:opacity-80 text-white rounded-full'>Read More</Link>
-                    </div>
+                <div className="flex items-center justify-center w-full mt-10">
+                    <Link href={"/services"} className='px-10 py-3 block bg-primary transition-all duration-500 hover:opacity-80 text-white rounded-full'>Read More</Link>
+                </div>
+            </div>
+
+            <div className="max-w-7xl w-full flex items-center justify-between md:flex-row flex-col gap-6 mx-auto p-5 md:mt-20 mt-10">
+                <div className="md:max-w-[50%] w-full">
+                    <h2 className='font-semibold leading-tight text-3xl'>Empowering Innovation through Automation Excellence</h2>
+                    <p className='text-zinc-500 md:text-lg mt-4'>We aim to revolutionize automation and manufacturing by leading the integration of manual processes with advanced technologies. With dedication and passion, we strive to propel businesses into a new era of efficiency and sustainable growth, fostering innovation in the industry.</p>
+                </div>
+                <div className="md:max-w-[40%] w-full">
+                    <Image src={img1} alt='img' />
+                </div>
+
             </div>
 
         </Wrapper>
